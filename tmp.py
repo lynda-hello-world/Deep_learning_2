2e34cc473f44161de6165ee75ea84888ea3ae2e7
lynda <hanlin7172003@yahoo.co.jp>
2023-10-25 21:31:47 +0900

    try TwoLayerNet

--- tmp.py
+++ tmp.py
@@ -1,16 +1,40 @@
 import numpy as np
-def sigmoid(x):
-    y=1/(1+np.exp(-x))
-    return y
 
-W1=np.random.randn(2,4)
-b1=np.random.randn(4,)
-W2=np.random.randn(4,3)
-b2=np.random.randn(3,)
+class Sigmoid:
+    def __init__(self):
+        self.params=[]
 
-x=np.random.randn(10,2)
-h=np.dot(x,W1)+b1
-a=sigmoid(h)
-y=np.dot(a,W2)+b2
+    def forward(self,x):
+        y=1/(1+np.exp(-x))
+        return y
 
-print(y)
+class Affine:
+    def __init__(self,W,b):
+        self.params=[W,b]
+
+    def forward(self,x):
+        W,b=self.params
+        out=np.dot(x,W)+b
+        return out
+
+class TwoLayerNet:
+    def __init__(self,input_size,hidden_size,out_size):
+        I,H,O=input_size,hidden_size,out_size
+        W1=np.random.randn(I,H)
+        b1=np.random.randn(H,)
+        W2=np.random.randn(H,O)
+        b2=np.random.randn(O,)
+        self.layers=[Affine(W1,b1),Sigmoid(),Affine(W2,b2)]
+        self.params=[]
+        for layer in self.layers:
+            self.params+=layer.params
+
+    def predict(self,x):
+        for layer in self.layers:
+            x=layer.forward(x)
+        return x
+
+x=np.random.randn(2,)
+model=TwoLayerNet(2,4,3)
+s=model.predict(x)
+print(s)
