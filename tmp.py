83108eafce1cc7ca93ab98c7e75ca0a46ddab0fe
lynda <hanlin7172003@yahoo.co.jp>
2023-10-26 15:04:29 +0900

    add MatMul class

--- tmp.py
+++ tmp.py
@@ -34,7 +34,19 @@ class TwoLayerNet:
             x=layer.forward(x)
         return x
 
-x=np.random.randn(2,)
-model=TwoLayerNet(2,4,3)
-s=model.predict(x)
-print(s)
+class MatMul:
+    def __init__(self,W):
+        self.params=[W]
+        self.grads=[np.zeros_like(W)]
+        self.x=None
+    def forward(self,x):
+        self.x=x
+        W,=self.params
+        out=np.dot(x,W)
+        return out
+    def backward(self,dout):
+        W,=self.params
+        dW=np.dot(self.x.T,dout)
+        dx=np.dot(dout,W.T)
+        self.grads[0][...]=dW
+        return dx
