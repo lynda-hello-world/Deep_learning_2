6affa2344369410b090a30ce294a7759e6068241
lynda <hanlin7172003@yahoo.co.jp>
2023-10-26 17:48:41 +0900

    add code for layers

--- layer.py
+++ layer.py
@@ -51,20 +51,25 @@ def cross_entropy_error(y, t):
     batch_size = y.shape[0]
     return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
-class SigmoidWithLoss:
+class SoftmaxWithLoss:
     def __init__(self):
         self.params, self.grads = [], []
-        self.loss = None
-        self.y = None  # sigmoidの出力
-        self.t = None  # 教師データ
+        self.y = None  # softmaxの出力
+        self.t = None  # 教師ラベル
     def forward(self, x, t):
         self.t = t
-        self.y = 1 / (1 + np.exp(-x))
-        self.loss = cross_entropy_error(np.c_[1 - self.y, self.y], self.t)
-        return self.loss
+        self.y = softmax(x)
+        # 教師ラベルがone-hotベクトルの場合、正解のインデックスに変換
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+        loss = cross_entropy_error(self.y, self.t)
+        return loss
     def backward(self, dout=1):
         batch_size = self.t.shape[0]
-        dx = (self.y - self.t) * dout / batch_size
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1
+        dx *= dout
+        dx = dx / batch_size
         return dx
 
 class TwoLayerNet:
